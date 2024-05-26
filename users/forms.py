f33e259d4754ef9b91b8a128b4f538eb51b84049
Bikalpa625 <bikalpashrestha1990@gmail.com>
2024-05-26 21:34:23 +0545

    register remove and start from begining

--- users/forms.py
+++ users/forms.py
@@ -1,22 +1,3 @@
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-class UserRegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100)
-    middle_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    username = forms.CharField(max_length=100  )
-    street_address = forms.CharField(max_length=255)
-    city = forms.CharField(max_length=100)
-    state = forms.CharField(max_length=100)
-    zip_code = forms.CharField(max_length=10)
-    mobile_number = forms.CharField(max_length=15)
-    
-
-    class Meta:
-        model=User
-        fields=['first_name','middle_name','last_name','username','password1','password2','street_address','city','state','zip_code','mobile_number']
 
 
 
--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
-from .forms import UserRegisterForm
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+
 
 class CustomLoginView(LoginView):
     template_name = 'users/login.html'
@@ -13,17 +13,10 @@ class CustomLoginView(LoginView):
         messages.error(self.request, 'Invalid username or password. Please try again.')
         return super().form_invalid(form)
     
+@login_required
 def register(request):
-    if request.method=='POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
-    else:
-        form=UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html')
+
 
 @login_required
 def logoutuser(request):
